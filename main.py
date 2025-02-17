1e371d64efc2172a1e2a4830639d8693dfbab92a
JetShark <JetShark@users.noreply.github.com>
2025-02-17 14:50:41 -0500

    Mask image with pixels of a specific color
    Use colour ranges for red & green to find potential apples in the image.
    Use the mask to return a new image with only pixels of those colours.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
 from PIL import Image
+import numpy as np
+import cv2
 import io
 
 app = FastAPI(title="Apple Image Processor")
@@ -10,6 +12,39 @@ def process_image(image: Image.Image) -> Image.Image:
     Stub for image processing. Currently just returns the original image.
     In implementation, this will contain the apple detection logic.
     """
+    # use HSV to detect red and green apples
+    # convert image to HSV
+    # image = image.convert('HSV')
+
+    hsv = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2HSV)
+
+    # define range of red color in HSV
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+
+    # define range of green color in HSV
+    lower_green = np.array([50,100,100])
+    upper_green = np.array([70,255,255])
+
+    # Threshold the HSV image to get only red colors
+    mask_red = cv2.inRange(hsv, lower_red, upper_red)
+
+    # Threshold the HSV image to get only green colors
+    mask_green = cv2.inRange(hsv, lower_green, upper_green)
+
+    # Bitwise-AND mask and original image
+    res_red = cv2.bitwise_and(hsv,hsv, mask= mask_red)
+    res_green = cv2.bitwise_and(hsv,hsv, mask= mask_green)
+
+    # merge both red and green masks into one image
+    res = cv2.bitwise_or(res_red, res_green)
+    
+    # convert image back to RGB
+    image = cv2.cvtColor(res, cv2.COLOR_HSV2RGB)
+
+    # convert numpy array to PIL image
+    image = Image.fromarray(image)
+
     return image
 
 @app.post("/is_apple", response_class=Response)
